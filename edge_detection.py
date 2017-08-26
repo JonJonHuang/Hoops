fdd0c461a9b38fd7c1dc0927ca8de52a6c1a342b
Eric Hemphill <hemphill.39@osu.edu>
2017-08-26 17:48:56 -0400

    added some edge detection with HoughLines

--- edge_detection.py
+++ edge_detection.py
@@ -6,10 +6,30 @@ import numpy as np
 import argparse
 import imutils
 import cv2
+from matplotlib import pyplot as plt
 
-img = cv2.imread('assets/images/still.png')
+img = cv2.flip(cv2.imread('assets/images/still_cropped.png'), 1)
+img = imutils.resize(img, width=min(960, img.shape[1]))
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-blurred = cv2.GaussianBlur(gray, (7, 7), 0)
-edged = cv2.Canny(blurred, 50, 100, 255)
+blurred = cv2.GaussianBlur(gray, (3, 3), 255)
+edges = cv2.Canny(blurred, 25, 150)
+hsv = hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+cv2.imwrite('edges.png', edges)
 
-cv2.imwrite('edges.png', edged)
+hist = cv2.calcHist([hsv[0]],[0],None,[179],[0,179])
+
+lines = cv2.HoughLines(edges, 1, np.pi/180, 225)
+for line in lines:
+    for rho,theta in line:
+        a = np.cos(theta)
+        b = np.sin(theta)
+        x0 = a*rho
+        y0 = b*rho
+        x1 = int(x0 + 1000*(-b))
+        y1 = int(y0 + 1000*(a))
+        x2 = int(x0 - 1000*(-b))
+        y2 = int(y0 - 1000*(a))
+
+        cv2.line(img,(x1,y1),(x2,y2),(0,0,255),1)
+
+cv2.imwrite('houghlines3.jpg',img)
