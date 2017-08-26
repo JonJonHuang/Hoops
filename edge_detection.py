042687379d334d77f3bbfa5c2d330520eb038fb1
Eric Hemphill <hemphill.39@osu.edu>
2017-08-26 14:52:43 -0400

    added edge detection and top down view of warriors court

---
+++ edge_detection.py
@@ -0,0 +1,15 @@
+# import the necessary packages
+from __future__ import print_function
+from imutils.object_detection import non_max_suppression
+from imutils import paths
+import numpy as np
+import argparse
+import imutils
+import cv2
+
+img = cv2.imread('assets/images/still.png')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+blurred = cv2.GaussianBlur(gray, (7, 7), 0)
+edged = cv2.Canny(blurred, 50, 100, 255)
+
+cv2.imwrite('edges.png', edged)
---
+++ tracking_people.py
@@ -0,0 +1,14 @@
+# import the necessary packages
+from __future__ import print_function
+from imutils.object_detection import non_max_suppression
+from imutils import paths
+import numpy as np
+import argparse
+import imutils
+import cv2
+
+
+# initialize the HOG descriptor/person detector
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
