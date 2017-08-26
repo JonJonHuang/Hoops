e5d9578c840896044a6a965a337d8d9d6a478422
Sayeef Moyen <moyen.2@buckeyemail.osu.edu>
2017-08-26 13:52:49 -0400

    more ball code

--- ball.py
+++ ball.py
@@ -5,4 +5,6 @@ import imutils
 import cv2
 
 ap = argparse.ArgumentParser()
-ap.add_argument("-v", "--video")
+ap.add_argument("-v", "--video", help="path to video file")
+ap.add_argument("-b", "--buffer", type=int, default=80, help="max buffer size")
+args = vars(ap.parse_args())
