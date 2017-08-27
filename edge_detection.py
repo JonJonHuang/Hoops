d9d1c3d70c3291b97b48dbdcb53a5b059702d1fb
Jon <jon55999@yahoo.com>
2017-08-26 20:46:34 -0400

    CAN GET COURT ASSUMING LINES ARE GOOD

--- edge_detection.py
+++ edge_detection.py
@@ -56,20 +56,26 @@ for line in lines:
         x2 = int(x0 - 1000*(-b))
         y2 = int(y0 - 1000*(a))
 
-        bs.append(y0)
-        slope = (y2-y1)/(x2-x1)
+        
+        slope = float(y2-y1)/float(x2-x1)
         slopes.append(slope)
+        b = y0 - (x0 * slope)
+        bs.append(b)
 
         cv2.line(img,(x1,y1),(x2,y2),(0,0,255),1)
 
 minSlopeIndex, maxSlopeIndex = minMaxIndex(slopes)
-minSlope = lines[minSlopeIndex]
-maxSlope = lines[maxSlopeIndex]
+minSlope = slopes[minSlopeIndex]
+maxSlope = slopes[maxSlopeIndex]
 minB = bs[minSlopeIndex]
 maxB = bs[maxSlopeIndex]
 
-for x in range(0, img.shape[1]):
-    for y in range(0, img.shape[0]):
-        if x < getX(maxSlopeLine)
+height = img.shape[0]
+for x in range(0, img.shape[1]-1):
+    for y in range(0, img.shape[0]-1):
+        if y < getY(minSlope, minB, x) or y < getY(maxSlope, maxB, x):
+            img[y][x][0] = 0
+            img[y][x][1] = 0
+            img[y][x][2] = 0
 
 cv2.imwrite('houghlines3.jpg',img)
